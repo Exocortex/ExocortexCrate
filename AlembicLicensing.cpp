66eb9e9a2d4b0b743cd5e6b5a7aa4d71636062fc
Ben Houston <ben@exocortex.com>
2012-02-03 17:01:58 -0500

    add licensing RLM and beta expiry.

---
+++ AlembicLicensing.cpp
@@ -0,0 +1,66 @@
+#include <time.h>
+#include "AlembicLicensing.h"
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int gLicenseToken = EC_LICENSE_RESULT_NO_LICENSE;
+
+#if defined( EXOCORTEX_RLM_ONLY )
+	#include "RlmSingletonDeclarations.h"
+#endif // EXOCORTEX_RLM_ONLY
+
+bool HasFullLicense()
+{
+	return ( GetLicense() == EC_LICENSE_RESULT_FULL_LICENSE );
+}
+
+int GetLicense()
+{
+	if( gLicenseToken == EC_LICENSE_RESULT_NO_LICENSE )
+	{
+		// default license status, could be overriden below.
+		gLicenseToken = EC_LICENSE_RESULT_DEMO_LICENSE;
+
+		// check RLM license first, so that users see that RLM is either used or not prior to expiry.	
+#if defined( EXOCORTEX_RLM_ONLY )
+		{
+			#pragma message( "Exocortex Licensing mode: RLM only" )
+			static string pluginName(PLUGIN_NAME);
+			ESS_LOG_INFO( "Looking for RLM license for " << pluginName << "..." );
+			Exocortex::RlmSingleton& rlmSingleton = Exocortex::RlmSingleton::getSingleton();
+
+			RlmProductID pluginLicenseIds2[] = PLUGIN_LICENSE_IDS;
+			vector<RlmProductID> rlmProductIds;
+			for( int i = 0; i < sizeof( pluginLicenseIds2 ) / sizeof( RlmProductID ); i ++ ) {
+				rlmProductIds.push_back( pluginLicenseIds2[i] );
+			}
+
+			if( rlmSingleton.checkoutLicense( "", pluginName, rlmProductIds ) ) {
+				gLicenseToken = EC_LICENSE_RESULT_FULL_LICENSE;
+				return gLicenseToken;
+			}
+		}
+#endif // EXOCORTEX_RLM_ONLY
+
+#if defined( EXOCORTEX_BETA_EXPIRY_DATE )
+		{
+			#pragma message( "Exocortex Licensing mode: Fixed expiry date" )
+			time_t now = time(NULL);
+			if( now <= EXOCORTEX_BETA_EXPIRY_DATE ) {  //http://unixtime-converter.com/
+				static string pluginName(PLUGIN_NAME);
+				ESS_LOG_WARNING( "Expiry date licensing is being used for " << pluginName );
+				gLicenseToken = EC_LICENSE_RESULT_FULL_LICENSE;
+				return gLicenseToken;
+			}
+		}
+#endif // Exocortex_BETA_EXPIRY_DATE
+
+	}
+
+	return gLicenseToken;
+}
+
+
---
+++ ExocortexServicesProxy.h
@@ -0,0 +1,59 @@
+#ifndef __EXOCORTEX_SERVICES_PROXY_H
+#define __EXOCORTEX_SERVICES_PROXY_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include "Python.h"
+
+#define EC_LICENSE_RESULT_NO_LICENSE	(0)
+#define EC_LICENSE_RESULT_DEMO_LICENSE	(1)
+#define EC_LICENSE_RESULT_FULL_LICENSE	(2)
+
+#define ESS_LOG_ERROR(a) do { std::stringstream s; s << a;  std::cerr << ( s.str().c_str() ); } while(0)
+#define ESS_LOG_WARNING(a) do { std::stringstream s; s << a; std::cout << (s.str().c_str() ); } while(0)
+#define ESS_LOG_INFO(a) do { std::stringstream s; s << a; std::cout << (s.str().c_str() ); } while(0)
+
+
+#ifndef EC_LOG_ERROR
+	#define EC_LOG_ERROR(a)		ESS_LOG_ERROR(a)
+#endif
+#ifndef EC_LOG_WARNING
+	#define EC_LOG_WARNING(a)	ESS_LOG_WARNING(a)
+#endif
+#ifndef EC_LOG_INFO
+	#define EC_LOG_INFO(a)		ESS_LOG_INFO(a)
+#endif
+#ifndef EC_ASSERT
+	#define EC_ASSERT(a)		
+#endif
+
+#if defined( EXOCORTEX_RLM_ONLY )
+
+	#ifndef PLUGIN_LICENSE_NAME
+		#error "PLUGIN_LICENSE_NAME not defined, required for EXOCORTEX_RLM_ONLY-style licensing"
+	#endif
+	#ifndef PLUGIN_LICENSE_VERSION
+		#error "PLUGIN_LICENSE_VERSION not defined, required for EXOCORTEX_RLM_ONLY-style licensing"
+	#endif
+
+	#ifndef PLUGIN_LICENSE_IDS
+		#define PLUGIN_LICENSE_IDS	{ RlmProductID( PLUGIN_LICENSE_NAME, PLUGIN_LICENSE_VERSION ) }
+	#endif
+
+	//#include <xsi_application.h>
+
+	#if defined( __GNUC__ )
+		#define printf_s(buffer, buffer_size, stringbuffer, ...) ( printf(buffer, stringbuffer, __VA_ARGS__) )
+ 		#define sprintf_s(buffer, buffer_size, stringbuffer, ...) ( sprintf(buffer, stringbuffer, __VA_ARGS__) )
+ 		#define vsprintf_s(buffer, buffer_size, stringbuffer, ...) ( vsprintf(buffer, stringbuffer, __VA_ARGS__) )
+	#endif
+ 
+	#include "RlmSingleton.h"
+
+#endif	// EXOCORTEX_RLM_ONLY
+
+
+
+#endif // __EXOCORTEX_SERVICES_PROXY_H
