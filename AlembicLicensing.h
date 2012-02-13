1b6351bbc945e56aafa035ef3c866bdc304fd331
Ben Houston <ben@exocortex.com>
2012-02-13 13:30:12 -0500

    Fixed #22, #32.

--- AlembicLicensing.h
+++ AlembicLicensing.h
@@ -1,13 +1,28 @@
 #ifndef __ALEMBIC_LICENSING_H
 #define __ALEMBIC_LICENSING_H
-#include <maya/MGlobal.h>
 
-// TODO: this has to be implemented properly
-inline bool HasFullLicense() { return true; }
 
-inline void EC_LOG_WARNING(MString message)
-{
-   MGlobal::displayWarning(message);
-}
+#define _EC_WSTR( s ) L ## s
+#define EC_WSTR( s ) _EC_WSTR( s )
+
+#define _EC_QUOTE( x ) #x
+#define EC_QUOTE( x ) _EC_QUOTE( x )
+
+
+
+// IMPORTANT, we are not using wide characters here.
+#define PLUGIN_NAME						"ExocortexAlembicMaya" EC_QUOTE( alembic_MAJOR_VERSION ) "." EC_QUOTE( alembic_MINOR_VERSION )
+#define PLUGIN_MAJOR_VERSION			alembic_MAJOR_VERSION
+#define PLUGIN_MINOR_VERSION			alembic_MINOR_VERSION
+#define PLUGIN_LICENSE_NAME				"alembic_maya"
+#define PLUGIN_LICENSE_VERSION			(alembic_MAJOR_VERSION*10)
+
+
+#include "ExocortexServicesProxy.h"
+
+extern int gLicenseToken;
+int GetLicense();
+bool HasFullLicense();
+
 
 #endif // __ALEMBIC_LICENSING_H
