a8dc30822efecdbb3cd8e0e8b79f69b8992cfa5e
Eric Lawless <eplawless+github@gmail.com>
2012-02-28 15:18:01 -0500

    Added build instructions for Houdini to README.md

--- build/build-vs2005_x64.bat
+++ build/build-vs2005_x64.bat
@@ -1,5 +1,5 @@
 rmdir /s vs2005_x64
 mkdir vs2005_x64
 cd vs2005_x64
-cmake -G "Visual Studio 8 2005 Win64" -DHDF5_ENABLE_THREADSAFE=OFF ..\..
+cmake -G "Visual Studio 8 2005 Win64" -DHDF5_ENABLE_THREADSAFE=OFF -DINCLUDE_ALEMBIC_HOUDINI=ON ..\..
 cd ..
