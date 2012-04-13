76a533f0d0f806298836d56d1a6d4c340f47ce79
Ben Houston <ben@exocortex.com>
2012-04-13 09:11:59 -0400

    minor updates to unix build scripts.

--- build/build-UnixRelease.sh
+++ build/build-UnixRelease.sh
@@ -1,4 +1,4 @@
 mkdir UnixRelease
 cd UnixRelease
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DHDF5_ENABLE_THREADSAFE=ON ../..
 cd ..
