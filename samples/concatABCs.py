034313b4b01e17160aaf82608ae5f5df00e31c40
Jack Caron <jacquelin.caron@gmail.com>
2012-06-07 12:05:44 -0400

    concatABCs that works (at least for XSI). - a simplified version where the files are only access twice instead of thrice - only the matrix for the Xforms are accumulated in memory, the rest is copied from archive to archive avoiding the needs of too much memory
    viewABC
    - added a filter to visit objects that contain that specific substring

--- samples/concatABCs.py
+++ samples/concatABCs.py
@@ -8,43 +8,61 @@ class ABCException(Exception):
    def __str__(self):
       return repr(self.value)
 
-concat_ts = []
-xforms_dict = {}
+concat_ts = []          # keep the time samples over all alembic files
+xforms_dict = {}        # keep the transformation matrix
+vertex_def = {}       # identify vertex transform properties in all alembic files
 
+# concat the time samples by adding add the end of the list and adding the 
 def concat_time_samples(append_ts):
    global concat_ts
    last = concat_ts[-1] # pick the last time sample currently known
    for ap in append_ts:
       concat_ts.append(ap + last)   # the time stamps must be added at the end
 
+# visit the object's properties and identify the vertex deforms one
+def extract_vertex_deform_info(prop, fullname, nb_ts):
+   global vertex_def
+   if prop.isCompound():
+      for sub_prop in prop.getPropertyNames():
+         extract_vertex_deform_info(prop.getProperty(sub_prop), fullname + "/" + sub_prop, nb_ts)
+   else:
+      if fullname not in vertex_def.keys():
+         vertex_def[fullname] = False
+      if not vertex_def[fullname]:                                      # this property needs to be rechecked
+         vertex_def[fullname] = (1 != prop.getNbStoredSamples())    # this property will need to be traited as a vertex transformation.
+
+# get the information about the Xforms and identify which properties are vertex deforms
 def extract_ts_xforms(archive):
    global xforms_dict
    global concat_ts
    concat_ts = concat_ts + list(archive.getSampleTimes()[1])   #get the first TS
-   nb_ts = len(concat_ts)
+   nb_ts = len(archive.getSampleTimes()[1])
+   print("\t\tsample times: " + str(nb_ts))
    for identifier in archive.getIdentifiers():
       obj = archive.getObject(identifier)
       o_type = obj.getType()
-      if o_type.find("Xform") == -1:
-         continue
       
-      xkey = identifier
-      xvalues = []
-      xfm = obj.getProperty(".xform")
-      
-      if xfm == None:
-         raise ABCException("no .xform in " + identifier)
-      
-      last_i = xfm.getNbStoredSamples()   # need to be sure that theirs one matrices per sampling
-      for i in xrange(0, last_i):
-         xvalues.append(xfm.getValues(i))
-      
-      if nb_ts != last_i:                 # duplicate the last matrix if necessary
-         last_v = xfm.getValues(last_i-1)
-         for i in xrange(last_i, nb_ts):
-            xvalues.append(last_v)
-      xforms_dict[xkey] = xvalues
-   pass
+      if o_type.find("Xform") > -1:    # is an Xform
+         xkey = identifier
+         xvalues = []
+         xfm = obj.getProperty(".xform")
+         
+         if xfm == None:
+            raise ABCException("no .xform in " + identifier)
+         
+         last_i = xfm.getNbStoredSamples()   # need to be sure that theirs one matrices per sampling
+         for i in xrange(0, last_i):
+            xvalues.append(xfm.getValues(i))
+         
+         if nb_ts != last_i:                 # duplicate the last matrix if necessary
+            last_v = xfm.getValues(last_i-1)
+            for i in xrange(last_i, nb_ts):
+               xvalues.append(last_v)
+         xforms_dict[xkey] = xvalues
+         
+      else:                            # still have to visit the object to identify vertex transform properties
+         for prop in obj.getPropertyNames():
+            extract_vertex_deform_info(obj.getProperty(prop), identifier + "/" + prop, nb_ts)
 
 def concat_ts_xforms(archive):
    global xforms_dict
@@ -52,84 +70,97 @@ def concat_ts_xforms(archive):
    arch_ts = archive.getSampleTimes()[1]  # archive's TS
    concat_time_samples(arch_ts)# add the TS right at the end of the list
    nb_ts = len(arch_ts)
+   print("\t\tsample times: " + str(nb_ts))
    nb_xforms = len(xforms_dict)           # need to be sure there's the same amount of Xforms in each file and that they're the same
    
    for identifier in archive.getIdentifiers():
       obj = archive.getObject(identifier)
       o_type = obj.getType()
-      if o_type.find("Xform") == -1:
-         continue
-      
-      xfm = obj.getProperty(".xform")
-      xvalues = xforms_dict[identifier]   # get the values for this identifier and test if it's a valid one!
-      if xvalues == None:
-         raise ABCException(identifier + " doesn't exist in previous files")
-      
-      last_i = xfm.getNbStoredSamples()   # need to be sure that theirs one matrices per sampling
-      for i in xrange(0, last_i):
-         xvalues.append(xfm.getValues(i))
-      
-      if nb_ts != last_i:                 # duplicate the last matrix if necessary
-         last_v = xfm.getValues(last_i-1)
-         for i in xrange(last_i, nb_ts):
-            xvalues.append(last_v)
-      
-      nb_xforms = nb_xforms - 1           # one more Xform done!
+      if o_type.find("Xform") > -1:
+         xfm = obj.getProperty(".xform")
+         xvalues = xforms_dict[identifier]   # get the values for this identifier and test if it's a valid one!
+         if xvalues == None:
+            raise ABCException(identifier + " doesn't exist in previous files")
+         
+         last_i = xfm.getNbStoredSamples()   # need to be sure that theirs one matrices per sampling
+         for i in xrange(0, last_i):
+            xvalues.append(xfm.getValues(i))
+         
+         if nb_ts != last_i:                 # duplicate the last matrix if necessary
+            last_v = xfm.getValues(last_i-1)
+            for i in xrange(last_i, nb_ts):
+               xvalues.append(last_v)
+         
+         nb_xforms = nb_xforms - 1           # one more Xform done!
+         
+      else:                            # still have to visit the object to identify vertex transform properties
+         for prop in obj.getPropertyNames():
+            extract_vertex_deform_info(obj.getProperty(prop), identifier + "/" + prop, nb_ts)
    
    if nb_xforms != 0:
       raise ABCException("The file is missing some xforms")
    pass
 
-
 # copy directly a property and its corresponding values
-def copy_property(prop, outProp, full_name):
-   for i in xrange(0, prop.getNbStoredSamples()):
-      vals = prop.getValues(i)
-      outProp.setValues(vals)
+def copy_property(prop, outProp, full_name, firstFile):
+   global vertex_def
+   full_name = full_name + "/" + prop.getName()
+   
+   # if it's the first file, then needs to copy it... or if it's a vertex deform, needs to append the values
+   if firstFile or vertex_def[full_name]:
+      for i in xrange(0, prop.getNbStoredSamples()):
+         outProp.setValues(prop.getValues(i))
 
 # visiting the structure, if it's a property, copy it, if it's a compound, continue the visit there
-def copy_compound_property(cprop, outCprop, full_name):
+def copy_compound_property(cprop, outCprop, full_name, firstFile):
    full_name = full_name + "/" + cprop.getName()
    for prop_name in cprop.getPropertyNames():
+      if prop_name == ".metadata":  # cause some problems, removed them
+         continue
       sub_prop = cprop.getProperty(prop_name)
       if sub_prop.isCompound():
-         copy_compound_property(sub_prop, outCprop.getProperty(prop_name, sub_prop.getType()), full_name)
+         copy_compound_property(sub_prop, outCprop.getProperty(prop_name, sub_prop.getType()), full_name, firstFile)
       else:
-         copy_property(sub_prop, outCprop.getProperty(prop_name, sub_prop.getType()), full_name)
+         copy_property(sub_prop, outCprop.getProperty(prop_name, sub_prop.getType()), full_name, firstFile)
 
 # similar to function "copy_objects" copyABC.py... only difference is that it replaces the Xforms with the ones accumulated
-def create_output(out_arch, in_arch):
+def create_output(out_arch, in_arch, firstFile):
    global concat_ts
    global xforms_dict
-   out_arch.createTimeSampling(concat_ts)
+   
+   nb_ts = len(in_arch.getSampleTimes())  # get the number of sample times
+   if firstFile:
+      out_arch.createTimeSampling(concat_ts) # assign the time sampling only once to the output archive
    
    for identifier in in_arch.getIdentifiers():
       obj = in_arch.getObject(identifier)
       o_type = obj.getType()
       out = out_arch.createObject(o_type, identifier)
-      out.setMetaData(obj.getMetaData())
+      if firstFile:
+         out.setMetaData(obj.getMetaData())
       
       if o_type.find("Xform") == -1:   # not an Xform, copy it integrally
-         
          for prop_name in obj.getPropertyNames():
+            if prop_name == ".metadata":  # cause some problems, removed them
+               continue
             prop = obj.getProperty(prop_name)
             if prop.isCompound():
-               copy_compound_property(prop, out.getProperty(prop_name, prop.getType()), identifier)
+               copy_compound_property(prop, out.getProperty(prop_name, prop.getType()), identifier, firstFile)
             else:
-               copy_property(prop, out.getProperty(prop_name, prop.getType()), identifier)
+               copy_property(prop, out.getProperty(prop_name, prop.getType()), identifier, firstFile)
          
-      else:                            # an Xform, use data collected above
-         
+      elif firstFile:                            # an Xform, use data collected above, but only for the first file, no need to repeat those
          xfm = out.getProperty(".xform", "matrix4d")
          xvalues = xforms_dict[identifier]
          for xval in xvalues:
             xfm.setValues(xval)
          
-   pass
 
 def main(args):
+   global concat_ts
+   global vertex_def
    # parser args
-   parser = argparse.ArgumentParser(description="Concatenate multiple alembic files only if their geometries are identical.")
+   parser = argparse.ArgumentParser(description="Concatenate multiple alembic files only if their geometries are identical.\n\nCurrently, it only supports Xforms. Issue #16 on GitHub is about adding vertex/normal deformations.")
    parser.add_argument("abc_files", metavar="{Alembic files}", type=str, nargs="+", help="alembic files to concatenate")
    parser.add_argument("-o", type=str, metavar="{Alembic output file}", help="optional output file name, default is \"a.abc\"", default="a.abc")
    ns = vars(parser.parse_args(args[1:]))
@@ -138,15 +169,16 @@ def main(args):
       print("Error: need at least two files for the concatenation")
       return
    
-   #read the first file and extract the time sampling and xforms
-   in_arch = alembic.getIArchive(ns["abc_files"][0])
-   print("\n\nScanning " + ns["abc_files"][0])
+   #read the first file and extract the time sampling, xforms and identifying vertex deforms
+   abc_files = ns["abc_files"]
+   in_arch = alembic.getIArchive(abc_files[0])
+   print("\n\nScanning " + abc_files[0])
    extract_ts_xforms(in_arch)
    in_arch = None
    
-   # go through the remaining files and accumulate the data about the xforms
+   # go through the remaining files and accumulate the data about the time sampling, xforms and identifying vertex deforms
    try:
-      for abc in ns["abc_files"][1:]:
+      for abc in abc_files[1:]:
          print("Scanning " + abc)
          concat_ts_xforms(alembic.getIArchive(abc))
    except ABCException as abc_error:
@@ -155,12 +187,20 @@ def main(args):
       return
    
    # create the concatenate archive
-   print("Creating " + ns["o"])
-   in_arch = alembic.getIArchive(ns["abc_files"][0])
+   print("\nCreating " + ns["o"])
+   print("Archive with " + str(len(concat_ts)) + " time sampling")
    out_arch = alembic.getOArchive(ns["o"])
-   create_output(out_arch, in_arch)
-   in_arch  = None
-   out_arch = None
+   
+   firstFile = True
+   for abc in abc_files:
+      print("\t>> " + abc)
+      in_arch = alembic.getIArchive(abc)
+      create_output(out_arch, in_arch, firstFile)
+      firstFile = False # not the first file anymore
+      in_arch = None    # better close it
+   
+   out_arch = None   # finally close the output archive
+   print("\n\n")
 
 if __name__ == "__main__":
    main(sys.argv)
--- samples/viewABC.py
+++ samples/viewABC.py
@@ -6,6 +6,7 @@ show_time = False
 show_meta = False
 show_size = False
 show_vals = False
+obj_filter = None
 
 def visit_prop(prop, depth):
    if prop.isCompound():
@@ -27,9 +28,13 @@ def visit_object(obj):
       visit_prop(obj.getProperty(prop), "  ")
 
 def visit_alembic(abc_archive):
+   global obj_filter
    if show_time:
       print("Time sampling: " + str(abc_archive.getSampleTimes()))
    for id in abc_archive.getIdentifiers():
+      if obj_filter != None and id.find(obj_filter) < 0:
+         continue          # pass over this object!
+      
       obj = abc_archive.getObject(id)
       print("OBJ: " + id + ", " + obj.getType())
       if show_meta:
@@ -41,6 +46,7 @@ def main(args):
    global show_meta
    global show_size
    global show_vals
+   global obj_filter
    
    # parser args
    parser = argparse.ArgumentParser(description="Explore the structure of an Alembic file.")
@@ -49,12 +55,14 @@ def main(args):
    parser.add_argument("-s", "--size", action='store_true', help='show only the number of values stored in the properties')
    parser.add_argument("-m", "--meta", action='store_true', help='show objects\' meta data')
    parser.add_argument("-t", "--time", action='store_true', help='show time sampling')
+   parser.add_argument("-f", "--filter", type=str, help="only show objects containing this substring in their identifier")
    ns = vars(parser.parse_args(args[1:]))
    
    show_time = ns["time"]
    show_meta = ns["meta"]
    show_size = ns["size"]
    show_vals = ns["vals"]
+   obj_filter = ns["filter"]
    
    abc_archive = alembic.getIArchive(ns["abc_in"])
    print("\n\nExploring " + ns["abc_in"])
