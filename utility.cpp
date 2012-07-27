bd0e72a7765720707ebb10f569bc033f99859042
Marshall Hahn <marshall@exocortex.com>
2012-07-27 16:03:53 -0400

    - disabled some uncessary warnings

--- utility.cpp
+++ utility.cpp
@@ -395,7 +395,7 @@ public:
 	void walkToChild(INode* node, int& childIndex)
 	{
 		if(!node){
-			ESS_LOG_INFO("walkToChild: node is null.");
+			//ESS_LOG_INFO("walkToChild: node is null.");
 			return;
 		}
 
@@ -421,7 +421,7 @@ public:
 		int enumCode = TREE_CONTINUE;
 
 		if(!node){
-			ESS_LOG_INFO("callback: node is null.");
+			//ESS_LOG_INFO("callback: node is null.");
 			return TREE_ABORT;
 		}
 
@@ -451,7 +451,7 @@ INode* GetNodeFromHierarchyPath(const std::string& path)
 	HierarchyPathResolver resolver(path);
     IScene *pScene = GET_MAX_INTERFACE()->GetScene();
 	if(!pScene){
-		ESS_LOG_INFO("pScene is null.");
+		//ESS_LOG_INFO("pScene is null.");
 		return NULL;
 	}
     pScene->EnumTree(&resolver);
