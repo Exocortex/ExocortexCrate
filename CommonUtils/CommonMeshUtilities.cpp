5a148a3a3aaf1a517882c97924410c6981446a92
Ben Houston <ben@exocortex.com>
2012-10-02 10:17:30 -0400

    fixed #23

--- CommonUtils/CommonMeshUtilities.cpp
+++ CommonUtils/CommonMeshUtilities.cpp
@@ -28,14 +28,14 @@ bool isAlembicMeshNormals( Alembic::AbcGeom::IObject *pIObj, bool& isConstant ) 
 
 	if(Alembic::AbcGeom::IPolyMesh::matches((*pIObj).getMetaData())) {
 		objMesh = Alembic::AbcGeom::IPolyMesh(*pIObj,Alembic::Abc::kWrapExisting);
-       if( objMesh.getSchema().getNormalsParam().valid() ) {
-			isConstant = objMesh.getSchema().getNormalsParam().isConstant();
-			return true;
+ 		if( objMesh.valid() ) {
+			if( objMesh.getSchema().getNormalsParam().valid() ) {
+				isConstant = objMesh.getSchema().getNormalsParam().isConstant();
+				return true;
+			}
 		}
 	}
-	else {
-		objSubD = Alembic::AbcGeom::ISubD(*pIObj,Alembic::Abc::kWrapExisting);
-	}
+
 
 	isConstant = true;
 	return false;
@@ -49,13 +49,17 @@ bool isAlembicMeshPositions( Alembic::AbcGeom::IObject *pIObj, bool& isConstant 
 
 	if(Alembic::AbcGeom::IPolyMesh::matches((*pIObj).getMetaData())) {
 		objMesh = Alembic::AbcGeom::IPolyMesh(*pIObj,Alembic::Abc::kWrapExisting);
-		isConstant = objMesh.getSchema().getPositionsProperty().isConstant();
-		return true;
+		if( objMesh.valid() ) {
+			isConstant = objMesh.getSchema().getPositionsProperty().isConstant();
+			return true;
+		}
 	}
 	else {
 		objSubD = Alembic::AbcGeom::ISubD(*pIObj,Alembic::Abc::kWrapExisting);
-		isConstant = objSubD.getSchema().getPositionsProperty().isConstant();
-		return true;
+		if( objSubD.valid() ) {
+			isConstant = objSubD.getSchema().getPositionsProperty().isConstant();
+			return true;
+		}
 	}
 	isConstant = true;
 	return false;
@@ -68,16 +72,20 @@ bool isAlembicMeshUVWs( Alembic::AbcGeom::IObject *pIObj, bool& isConstant ) {
 
 	if(Alembic::AbcGeom::IPolyMesh::matches((*pIObj).getMetaData())) {
 		objMesh = Alembic::AbcGeom::IPolyMesh(*pIObj,Alembic::Abc::kWrapExisting);
-		if( objMesh.getSchema().getUVsParam().valid() ) {
-			isConstant = objMesh.getSchema().getUVsParam().isConstant();
-			return true;
+		if( objMesh.valid() ) {
+			if( objMesh.getSchema().getUVsParam().valid() ) {
+				isConstant = objMesh.getSchema().getUVsParam().isConstant();
+				return true;
+			}
 		}
 	}
 	else {
 		objSubD = Alembic::AbcGeom::ISubD(*pIObj,Alembic::Abc::kWrapExisting);
-		if( objSubD.getSchema().getUVsParam().valid() ) {
-			isConstant = objSubD.getSchema().getUVsParam().isConstant();
-			return true;
+		if( objSubD.valid() ) {
+			if( objSubD.getSchema().getUVsParam().valid() ) {
+				isConstant = objSubD.getSchema().getUVsParam().isConstant();
+				return true;
+			}
 		}
 	}
 	isConstant = true;
@@ -108,7 +116,7 @@ bool isAlembicMeshTopoDynamic( Alembic::AbcGeom::IObject *pIObj ) {
 			hasDynamicTopo = !faceCountProp.isConstant();
 		}
 	}
-	else
+	else if( subDSample.valid() )
 	{
 		Alembic::Abc::IInt32ArrayProperty faceCountProp = Alembic::Abc::IInt32ArrayProperty(objSubD.getSchema(),".faceCounts");
 		if(faceCountProp.valid()) {
@@ -130,15 +138,12 @@ bool isAlembicMeshPointCache( Alembic::AbcGeom::IObject *pIObj ) {
 		objSubD = Alembic::AbcGeom::ISubD(*pIObj,Alembic::Abc::kWrapExisting);
 	}
 
-	Alembic::AbcGeom::IPolyMeshSchema::Sample polyMeshSample;
-	Alembic::AbcGeom::ISubDSchema::Sample subDSample;
-
 	bool isPointCache = false;
 	if(objMesh.valid())
 	{
 		Alembic::Abc::IInt32ArrayProperty faceCountProp = Alembic::Abc::IInt32ArrayProperty(objMesh.getSchema(),".faceCounts");
 		if( ! faceCountProp.valid() ) {
-			Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objMesh.getSchema(),".P");
+			Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objMesh.getSchema(),"P");
 			if( positionsProp.valid() && positionsProp.getValue()->size() > 0 ) {
 				isPointCache = true;
 			}
@@ -147,18 +152,18 @@ bool isAlembicMeshPointCache( Alembic::AbcGeom::IObject *pIObj ) {
 			Alembic::Abc::Int32ArraySamplePtr faceCounts = faceCountProp.getValue();
 			// the first is our preferred method, the second check here is Helge's method that is now considered obsolete
 			if( faceCounts->size() == 0 || ( faceCounts->size() == 1 && ( faceCounts->get()[0] == 0 ) ) ) {
-				Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objMesh.getSchema(),".P");
+				Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objMesh.getSchema(),"P");
 				if( positionsProp.valid() && positionsProp.getValue()->size() > 0 ) {
 					isPointCache = true;
 				}
 			}
 		}
 	}
-	else
+	else if( objSubD.valid() )
 	{
 		Alembic::Abc::IInt32ArrayProperty faceCountProp = Alembic::Abc::IInt32ArrayProperty(objSubD.getSchema(),".faceCounts");		
 		if( ! faceCountProp.valid() ) {
-			Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objMesh.getSchema(),".P");
+			Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objSubD.getSchema(),"P");
 			if( positionsProp.valid() && positionsProp.getValue()->size() > 0 ) {
 				isPointCache = true;
 			}
@@ -167,7 +172,7 @@ bool isAlembicMeshPointCache( Alembic::AbcGeom::IObject *pIObj ) {
 			Alembic::Abc::Int32ArraySamplePtr faceCounts = faceCountProp.getValue();
 			// the first is our preferred method, the second check here is Helge's method that is now considered obsolete
 			if( faceCounts->size() == 0 || ( faceCounts->size() == 1 && ( faceCounts->get()[0] == 0 ) ) ) {
-				Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objSubD.getSchema(),".P");
+				Alembic::Abc::IP3fArrayProperty positionsProp = Alembic::Abc::IP3fArrayProperty(objSubD.getSchema(),"P");
 				if( positionsProp.valid() && positionsProp.getValue()->size() > 0 ) {
 					isPointCache = true;
 				}
