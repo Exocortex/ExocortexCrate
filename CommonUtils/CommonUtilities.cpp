51ae0571b036404791b7c5fab029082ae49157a3
Ben Houston <ben@exocortex.com>
2012-10-11 16:40:33 -0400

    fixed #30.

--- CommonUtils/CommonUtilities.cpp
+++ CommonUtils/CommonUtilities.cpp
@@ -13,6 +13,216 @@ stats_map default_stats_policy::stats;
 #include <string>
 #include <boost/algorithm/string.hpp>
 
+struct AlembicObjectInfo
+{
+	AlembicObjectInfo() {
+		numSamples = -1;
+		isMeshPointCache = -1;
+		isMeshTopoDynamic = -1;
+	}
+
+	Alembic::Abc::IObject obj;
+	int numSamples;
+	int isMeshPointCache;
+	int isMeshTopoDynamic;
+	std::vector<std::string> childIdentifiers;
+};
+
+typedef std::map<std::string,AlembicObjectInfo> StringObjectInfoMap;
+typedef std::pair<std::string,AlembicObjectInfo> StringObjectInfoPair;
+
+struct AlembicArchiveInfo
+{
+   Alembic::Abc::IArchive * archive;
+   int refCount;
+
+   AlembicArchiveInfo()
+   {
+      archive = NULL;
+      refCount = 0;
+   }
+
+   StringObjectInfoMap identifierToObjectInfo;
+};
+
+std::map<std::string,AlembicArchiveInfo> gArchives;
+
+std::string resolvePath( std::string originalPath ) {
+   static std::map<std::string,std::string> s_originalToResolvedPath;
+
+   if( s_originalToResolvedPath.find( originalPath ) != s_originalToResolvedPath.end() ) {
+	   return s_originalToResolvedPath[ originalPath ];
+   }
+
+   std::string resolvedPath = resolvePath_Internal( originalPath );
+
+   s_originalToResolvedPath.insert( std::pair<std::string,std::string>( originalPath, resolvedPath ) );
+
+   return resolvedPath;
+}
+
+Alembic::Abc::IArchive * getArchiveFromID(std::string path)
+{
+   std::string resolvedPath = resolvePath(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   if(it == gArchives.end())
+   {
+     // check if the file exists
+     if( ! HasAlembicReaderLicense() )
+     {
+         if(gArchives.size() == 1)
+         {
+            ESS_LOG_ERROR("[ExocortexAlembic] Reader license not found: Only one open archive at a time allowed!");
+            return NULL;
+         }
+      }
+   
+	  FILE * file = fopen(resolvedPath.c_str(),"rb");
+      if(file != NULL)
+      {
+         fclose(file);
+         addArchive(new Alembic::Abc::IArchive( Alembic::AbcCoreHDF5::ReadArchive(), resolvedPath));
+         return getArchiveFromID(resolvedPath);
+      }
+      return NULL;
+   }
+   return it->second.archive;
+}
+
+bool archiveExists(std::string path)
+{
+   std::string resolvedPath = resolvePath(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   return it != gArchives.end();
+}
+
+std::string addArchive(Alembic::Abc::IArchive * archive)
+{
+   AlembicArchiveInfo info;
+   info.archive = archive;
+   gArchives.insert(std::pair<std::string,AlembicArchiveInfo>(archive->getName(),info));
+   return archive->getName().c_str();
+}
+
+void deleteArchive(std::string path)
+{
+   std::string resolvedPath = resolvePath(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   if(it == gArchives.end())
+      return;
+   it->second.archive->reset();
+   delete(it->second.archive);
+   gArchives.erase(it);
+}
+
+void deleteAllArchives()
+{
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   for(it = gArchives.begin(); it != gArchives.end(); it++)
+   {
+      it->second.archive->reset();
+      delete(it->second.archive);
+   }
+   gArchives.clear();
+}
+
+AlembicObjectInfo* getObjectInfoFromArchive(std::string path, std::string identifier)
+{
+   Alembic::Abc::IArchive * archive = getArchiveFromID(path);
+   if(archive == NULL)
+      return NULL;
+
+   if(identifier[0] != '/'){
+		return NULL;
+   }
+
+   std::string resolvedPath = resolvePath(path);
+
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+
+   StringObjectInfoMap &identifierToObjectInfo = (*it).second.identifierToObjectInfo;
+   if( identifierToObjectInfo.find( identifier ) != identifierToObjectInfo.end() ) {
+	   return &(identifierToObjectInfo[identifier]);
+   }
+
+   // split the path
+   std::vector<std::string> parts;
+   boost::split(parts, identifier, boost::is_any_of("/"));
+
+   // recurse to find it
+   Alembic::Abc::IObject obj = archive->getTop();
+   for(size_t i=1;i<parts.size();i++)
+   {
+      Alembic::Abc::IObject child(obj,parts[i]);
+      obj = child;
+      if(!obj)
+      {
+         obj = Alembic::Abc::IObject();
+         break;
+      }
+   }
+
+   AlembicObjectInfo objectInfo;
+   objectInfo.obj = obj;
+
+   identifierToObjectInfo.insert( StringObjectInfoPair( identifier, objectInfo ) );
+
+   return &(identifierToObjectInfo[identifier]);
+}
+Alembic::Abc::IObject getObjectFromArchive(std::string path, std::string identifier)
+{
+	AlembicObjectInfo* objectInfo = getObjectInfoFromArchive(path,identifier );
+	if( objectInfo == NULL ) {
+		return Alembic::Abc::IObject();
+	}
+	return objectInfo->obj;
+}
+int addRefArchive(std::string path)
+{
+   if(path.empty())
+      return -1;
+   std::string resolvedPath = resolvePath(path);
+
+   // call get archive to ensure to create it!
+   getArchiveFromID(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   if(it == gArchives.end())
+      return -1;
+   it->second.refCount++;
+   return it->second.refCount;
+}
+
+int delRefArchive(std::string path)
+{
+   std::string resolvedPath = resolvePath(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   if(it == gArchives.end())
+      return -1;
+   it->second.refCount--;
+   if(it->second.refCount==0)
+   {
+      deleteArchive(resolvedPath);
+      return 0;
+   }
+   return it->second.refCount;
+}
+
+int getRefArchive(std::string path)
+{
+   std::string resolvedPath = resolvePath(path);
+   std::map<std::string,AlembicArchiveInfo>::iterator it;
+   it = gArchives.find(resolvedPath);
+   if(it == gArchives.end())
+      return -1;
+   return it->second.refCount;
+}
+
 bool validate_filename_location(const char *filename)
 {
    std::ofstream fout(filename);
