7d449e1d63457964abbd6a0c78b899b84c80ebad
Ben Houston <ben@exocortex.com>
2012-10-11 16:39:47 -0400

    fixed #274

--- AlembicArchiveStorage.h
+++ AlembicArchiveStorage.h
@@ -4,17 +4,8 @@
 #include "Foundation.h"
 #include "AlembicMax.h"
 #include <map>
+#include "CommonUtilities.h"
 
-bool archiveExists(std::string path);
-Alembic::Abc::IArchive * getArchiveFromID(std::string path);
-std::string addArchive(Alembic::Abc::IArchive * archive);
-void deleteArchive(std::string path);
-void deleteAllArchives();
-Alembic::Abc::IObject getObjectFromArchive(std::string path, std::string identifier);
 
-// ref counting
-int addRefArchive(std::string path);
-int delRefArchive(std::string path);
-int getRefArchive(std::string path);
 
 #endif
