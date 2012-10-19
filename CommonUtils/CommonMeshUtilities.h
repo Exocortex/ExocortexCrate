62492ffaca1588111f27da2181248eb2e0d8896a
Ben Houston <ben@exocortex.com>
2012-10-19 08:23:41 -0400

    fixed #42

--- CommonUtils/CommonMeshUtilities.h
+++ CommonUtils/CommonMeshUtilities.h
@@ -1,6 +1,9 @@
 #ifndef __MESH_UTILITIES_H
 #define __MESH_UTILITIES_H
 
+
+namespace AbcA = ::Alembic::AbcCoreAbstract::ALEMBIC_VERSION_NS;
+
 bool isAlembicMeshValid( Alembic::AbcGeom::IObject *pIObj );
 bool isAlembicMeshNormals( Alembic::AbcGeom::IObject *pIObj, bool& isConstant );
 bool isAlembicMeshPositions( Alembic::AbcGeom::IObject *pIObj, bool& isConstant );
@@ -14,5 +17,9 @@ int validateAlembicMeshTopo(std::vector<Alembic::AbcCoreAbstract::ALEMBIC_VERSIO
 							std::vector<Alembic::AbcCoreAbstract::ALEMBIC_VERSION_NS::int32_t> faceIndices,
 							const std::string& meshName);
 
+bool getIndexAndValues( Alembic::Abc::Int32ArraySamplePtr faceIndices, Alembic::AbcGeom::IV2fGeomParam& param, AbcA::index_t sampleIndex, 
+					   std::vector<Imath::V2f>& outputValues, std::vector<AbcA::uint32_t>& outputIndices );
+bool getIndexAndValues( Alembic::Abc::Int32ArraySamplePtr faceIndices, Alembic::AbcGeom::IN3fGeomParam& param, AbcA::index_t sampleIndex,
+					   std::vector<Imath::V3f>& outputValues, std::vector<AbcA::uint32_t>& outputIndices );
 
 #endif // __MESH_UTILITIES_H
