32b613690d14ee08eaeb4d3df47c506b9ab13fbc
Ben Houston <ben@exocortex.com>
2012-10-29 10:26:08 -0400

    minor compile bug.

--- AlembicLicensing.h
+++ AlembicLicensing.h
@@ -11,11 +11,11 @@
 
 
 // IMPORTANT, we are not using wide characters here.
-#define PLUGIN_NAME						"ExocortexAlembicArnold" EC_QUOTE( alembic_MAJOR_VERSION ) "." EC_QUOTE( alembic_MINOR_VERSION )
-#define PLUGIN_MAJOR_VERSION			alembic_MAJOR_VERSION
-#define PLUGIN_MINOR_VERSION			alembic_MINOR_VERSION
+#define PLUGIN_NAME						"ExocortexAlembicArnold" EC_QUOTE( crate_MAJOR_VERSION ) "." EC_QUOTE( crate_MINOR_VERSION )
+#define PLUGIN_MAJOR_VERSION			crate_MAJOR_VERSION
+#define PLUGIN_MINOR_VERSION			crate_MINOR_VERSION
 #define PLUGIN_LICENSE_NAME				"alembic_arnold"
-#define PLUGIN_LICENSE_VERSION			(alembic_MAJOR_VERSION*10)
+#define PLUGIN_LICENSE_VERSION			(crate_MAJOR_VERSION*10)
 
 #define PLUGIN_LICENSE_IDS	{ RlmProductID( PLUGIN_LICENSE_NAME, PLUGIN_LICENSE_VERSION ), RlmProductID( "alembic_reader", PLUGIN_LICENSE_VERSION ), RlmProductID( "alembic_softimage", PLUGIN_LICENSE_VERSION ), RlmProductID( "alembic_maya", PLUGIN_LICENSE_VERSION ), RlmProductID( "alembic", PLUGIN_LICENSE_VERSION ) }
 
