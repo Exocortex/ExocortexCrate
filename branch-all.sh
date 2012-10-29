fda64384d4e90efa94bba24eec245fd72667de5f
Jack Caron <jacquelin.caron@gmail.com>
2012-10-29 14:06:42 -0400

    linux compatibility fixes.

--- branch-all.sh
+++ branch-all.sh
@@ -1,27 +1,27 @@
 cd ..
 
 cd ExocortexAlembicShared
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembicArnold
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembicPython
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembicMaya
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembicSoftimage
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembic3DSMax
-git fetch $1
+git branch $1
 cd ..
 
 cd ExocortexAlembicShared
--- tag-all.sh
+++ tag-all.sh
@@ -1,27 +1,27 @@
 cd ..
 
 cd ExocortexAlembicShared
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembicArnold
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembicPython
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembicMaya
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembicSoftimage
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembic3DSMax
-git tag %1
+git tag $1
 cd ..
 
 cd ExocortexAlembicShared
