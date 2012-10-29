f54f5b4abb2d26313c65d551e115fc339fd8d595
Ben Houston <ben@exocortex.com>
2012-10-29 13:31:12 -0400

    further fixes to #58

---
+++ merge-all.sh
@@ -0,0 +1,27 @@
+cd ..
+
+cd ExocortexAlembicShared
+git merge %1
+cd ..
+
+cd ExocortexAlembicArnold
+git merge %1
+cd ..
+
+cd ExocortexAlembicPython
+git merge %1
+cd ..
+
+cd ExocortexAlembicMaya
+git merge %1
+cd ..
+
+cd ExocortexAlembicSoftimage
+git merge %1
+cd ..
+
+cd ExocortexAlembic3DSMax
+git merge %1
+cd ..
+
+cd ExocortexAlembicShared
