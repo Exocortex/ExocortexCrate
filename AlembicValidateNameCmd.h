cadd7c4e5511bc01185c60714619521cae86252b
Jack Caron <jacquelin.caron@gmail.com>
2012-11-26 14:59:05 -0500

    fixed #215

--- AlembicValidateNameCmd.h
+++ AlembicValidateNameCmd.h
@@ -3,21 +3,6 @@
 
   #include "AlembicObject.h"
 
-	/*
-  class AlembicValidateNameCommand : public MPxCommand
-  {
-  public:
-    AlembicValidateNameCommand() {}
-    virtual ~AlembicValidateNameCommand()  {}
-
-    virtual bool isUndoable() const { return false; }
-    MStatus doIt(const MArgList& args);
-
-    static MSyntax createSyntax();
-    static void* creator() { return new AlembicValidateNameCommand(); }
-  };
-	//*/
-
   class AlembicAssignFacesetCommand : public MPxCommand
   {
   public:
@@ -43,20 +28,5 @@
     static MSyntax createSyntax();
     static void* creator() { return new AlembicAssignInitialSGCommand(); }
   };
-
-	/*
-  class AlembicFileRefCountCommand : public MPxCommand
-  {
-  public:
-    AlembicFileRefCountCommand() {}
-    virtual ~AlembicFileRefCountCommand()  {}
-
-    virtual bool isUndoable() const { return false; }
-    MStatus doIt(const MArgList& args);
-
-    static MSyntax createSyntax();
-    static void* creator() { return new AlembicFileRefCountCommand(); }
-  };
-	//*/
 
 #endif
