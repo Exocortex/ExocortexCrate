5fc50d237af887288a8562b1ffd2c960bb52a736
jcaron <jcaron@BuildServer.(none)>
2013-02-20 14:57:32 -0500

    check for visibility!

--- CommonUtils/CommonUtilities.h
+++ CommonUtils/CommonUtilities.h
@@ -216,6 +216,7 @@ struct BasicSchemaData
 	};
 	SCHEMA_TYPE type;
 	bool isConstant;
+	bool isVisibilityAnimated;
 	size_t nbSamples;
 };
 bool getBasicSchemaDataFromObject(Alembic::Abc::IObject &object, BasicSchemaData &bsd);
