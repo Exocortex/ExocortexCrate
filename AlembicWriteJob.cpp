62f0c99bc1d4f00f78fe71bcbe3fd27ae37c0427
Marshall Hahn <marshall@exocortex.com>
2013-05-22 15:19:55 -0400

    owgaw

--- AlembicWriteJob.cpp
+++ AlembicWriteJob.cpp
@@ -128,7 +128,8 @@ CStatus AlembicWriteJob::PreProcess()
    try
    {
       mArchive = CreateArchiveWithInfo(
-		  Alembic::AbcCoreHDF5::WriteArchive( true ),
+		  //Alembic::AbcCoreHDF5::WriteArchive( true ),
+          Alembic::AbcCoreOgawa::WriteArchive(),
             mFileName.GetAsciiString(),
             getExporterName( "Softimage " EC_QUOTE( crate_Softimage_Version ) ).c_str(),
 			getExporterFileName( sceneFileName.GetAsciiString() ).c_str(),
