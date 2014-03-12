6d1404915afb24dccbc59ba9fe8a24244de39075
Marshall Hahn <marshall@exocortex.com>
2014-03-12 16:54:29 -0400

    arnold 4.1.0.0 (Issue #392)

--- stdafx.h
+++ stdafx.h
@@ -2,8 +2,34 @@
 #define _ARNOLD_ALEMBIC_FOUNDATION_H_
 
 
-#include <ai.h>
-#include "CommonAlembic.h"
+   #include <ai.h>
+   #include "CommonAlembic.h"
+
+   #ifndef AtULong
+
+      typedef void               AtVoid;
+      typedef char               AtChar;
+      typedef char               AtBoolean;
+
+      typedef unsigned short     AtUShort;
+      typedef short              AtShort;
+      typedef unsigned long      AtULong;
+      typedef long               AtLong;
+
+      typedef signed long long   AtLLong;
+      typedef unsigned long long AtULLong;
 
 
-#endif
+      typedef int                AtInt;
+      typedef unsigned int       AtUInt;
+
+      typedef float              AtFloat;
+      typedef double             AtDouble;
+
+      typedef const AtChar*      AtConstCharPtr;
+      typedef AtVoid*            AtVoidPtr;
+
+   #endif
+
+
+#endif 
