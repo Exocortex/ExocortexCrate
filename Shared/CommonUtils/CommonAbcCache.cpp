666803ec2d3158b478fb5c42a7ab1328096c6f68
Ben Houston <ben@exocortex.com>
2015-08-03 12:16:23 -0400

    fix linux build.

--- Shared/CommonUtils/CommonAbcCache.cpp
+++ Shared/CommonUtils/CommonAbcCache.cpp
@@ -85,10 +85,12 @@ AbcObjectCache* addObjectToCache( AbcArchiveCache* fullNameToObjectCache, Abc::I
 bool s_hasRunOnceRun = false;
 
 void runonce() {
+#ifdef _MSC_VER
 	if( ! s_hasRunOnceRun ) {
 		_setmaxstdio( 2048 );
 		s_hasRunOnceRun = true;
 	}
+#endif
 }
 
 
