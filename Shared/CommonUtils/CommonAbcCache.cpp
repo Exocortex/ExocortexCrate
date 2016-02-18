Exocortex/ExocortexCrate

commit 966e6049507dda1595ee17104189e6e4eb4aa1fa

Cleaned up CommonUtils code with clang-format

Shared/CommonUtils/CommonAbcCache.cpp
@@ -1,105 +1,104 @@
+#include "CommonAbcCache.h"
 #include "CommonAlembic.h"
 #include "CommonMeshUtilities.h"
 #include "CommonUtilities.h"
-#include "CommonAbcCache.h"
 
-
-AbcObjectCache::AbcObjectCache( Alembic::Abc::IObject & objToCache )
-		:	obj( objToCache ), isMeshTopoDynamic(false), isMeshPointCache(false), fullName(objToCache.getFullName())
+AbcObjectCache::AbcObjectCache(Alembic::Abc::IObject& objToCache)
+    : obj(objToCache),
+      isMeshTopoDynamic(false),
+      isMeshPointCache(false),
+      fullName(objToCache.getFullName())
 {
-	ESS_PROFILE_SCOPE("AbcObjectCache::AbcObjectCache");
+  ESS_PROFILE_SCOPE("AbcObjectCache::AbcObjectCache");
 
-	BasicSchemaData bsd;
-	getBasicSchemaDataFromObject(objToCache, bsd);
-	isConstant = bsd.isConstant;
-	numSamples = bsd.nbSamples;
-	bool isMesh = true;
-	if ( bsd.type == bsd.__POLYMESH || !( isMesh = (bsd.type != bsd.__SUBDIV) ) )
-	{
-		bool isTopoDyn = false;
-		extractMeshInfo(&objToCache, isMesh, isMeshPointCache, isTopoDyn);
-		if (!isConstant)
-			isMeshTopoDynamic = isTopoDyn;
-	}
+  BasicSchemaData bsd;
+  getBasicSchemaDataFromObject(objToCache, bsd);
+  isConstant = bsd.isConstant;
+  numSamples = bsd.nbSamples;
+  bool isMesh = true;
+  if (bsd.type == bsd.__POLYMESH || !(isMesh = (bsd.type != bsd.__SUBDIV))) {
+    bool isTopoDyn = false;
+    extractMeshInfo(&objToCache, isMesh, isMeshPointCache, isTopoDyn);
+    if (!isConstant) isMeshTopoDynamic = isTopoDyn;
+  }
 }
 
+AbcObjectCache::~AbcObjectCache() {}
+IXformPtr AbcObjectCache::getXform()
+{
+  if (!pObjXform && obj.valid() && AbcG::IXform::matches(obj.getMetaData())) {
+    pObjXform = IXformPtr(new AbcG::IXform(obj, Abc::kWrapExisting));
 
- AbcObjectCache::~AbcObjectCache() {
-
- }
-
-IXformPtr AbcObjectCache::getXform(){
-
-   if(!pObjXform && obj.valid() && AbcG::IXform::matches(obj.getMetaData())){
-      pObjXform = IXformPtr(new AbcG::IXform(obj, Abc::kWrapExisting));
-
-      //iXformVec.resize(numSamples);
-      //for(int i=0; i<iXformVec.size(); i++){
-      //   AbcG::XformSample sample;
-      //   pObjXform->getSchema().get(sample, i);
-      //   iXformVec[i] = sample.getMatrix();
-      //}
-   }
-   return pObjXform;
+    // iXformVec.resize(numSamples);
+    // for(int i=0; i<iXformVec.size(); i++){
+    //   AbcG::XformSample sample;
+    //   pObjXform->getSchema().get(sample, i);
+    //   iXformVec[i] = sample.getMatrix();
+    //}
+  }
+  return pObjXform;
 }
-
 
 Abc::M44d AbcObjectCache::getXformMatrix(int index)
 {
-   if(iXformMap.find(index) == iXformMap.end()){
-      AbcG::XformSample sample;
-      getXform();
-      pObjXform->getSchema().get(sample, index);
-      Abc::M44d mat = sample.getMatrix();
-      iXformMap[index] = mat;
-      return mat;
-   }
-   return iXformMap[index];
+  if (iXformMap.find(index) == iXformMap.end()) {
+    AbcG::XformSample sample;
+    getXform();
+    pObjXform->getSchema().get(sample, index);
+    Abc::M44d mat = sample.getMatrix();
+    iXformMap[index] = mat;
+    return mat;
+  }
+  return iXformMap[index];
 }
 
+AbcObjectCache* addObjectToCache(AbcArchiveCache* fullNameToObjectCache,
+                                 Abc::IObject& obj,
+                                 std::string parentIdentifier,
+                                 CommonProgressBar* pBar)
+{
+  ESS_PROFILE_SCOPE("addObjectToCache");
+  AbcObjectCache objectCache(obj);
+  objectCache.parentIdentifier = parentIdentifier;
+  for (int i = 0; i < obj.getNumChildren(); i++) {
+    if (pBar) {
+      pBar->incr(1);
+      if (!(i % 20) && pBar->isCancelled()) return 0;
+    }
 
+    Abc::IObject child = obj.getChild(i);
+    AbcObjectCache* childObjectCache = addObjectToCache(
+        fullNameToObjectCache, child, objectCache.fullName, pBar);
+    if (childObjectCache == 0) return 0;
 
-AbcObjectCache* addObjectToCache( AbcArchiveCache* fullNameToObjectCache, Abc::IObject &obj, std::string parentIdentifier, CommonProgressBar *pBar ) {
-	ESS_PROFILE_SCOPE("addObjectToCache");
-	AbcObjectCache objectCache( obj );
-	objectCache.parentIdentifier = parentIdentifier;
-	for( int i = 0; i < obj.getNumChildren(); i ++ ) {
-		if (pBar)
-		{
-			pBar->incr(1);
-			if (!(i % 20) && pBar->isCancelled())	
-				return 0;
-		}
-
-		Abc::IObject child = obj.getChild( i );
-		AbcObjectCache* childObjectCache = addObjectToCache( fullNameToObjectCache, child, objectCache.fullName, pBar );
-		if (childObjectCache == 0)
-			return 0;
-
-		objectCache.childIdentifiers.push_back( childObjectCache->fullName );
-	}
-	fullNameToObjectCache->insert( AbcArchiveCache::value_type( objectCache.fullName, objectCache ) );
-	return &( fullNameToObjectCache->find( objectCache.fullName )->second );
+    objectCache.childIdentifiers.push_back(childObjectCache->fullName);
+  }
+  fullNameToObjectCache->insert(
+      AbcArchiveCache::value_type(objectCache.fullName, objectCache));
+  return &(fullNameToObjectCache->find(objectCache.fullName)->second);
 }
 
 bool s_hasRunOnceRun = false;
 
-void runonce() {
+void runonce()
+{
 #ifdef _MSC_VER
-	if( ! s_hasRunOnceRun ) {
-		_setmaxstdio( 2048 );
-		s_hasRunOnceRun = true;
-	}
+  if (!s_hasRunOnceRun) {
+    _setmaxstdio(2048);
+    s_hasRunOnceRun = true;
+  }
 #endif
 }
 
+bool createAbcArchiveCache(Abc::IArchive* pArchive,
+                           AbcArchiveCache* fullNameToObjectCache,
+                           CommonProgressBar* pBar)
+{
+  ESS_PROFILE_SCOPE("createAbcArchiveCache");
+  EC_LOG_INFO("Creating AbcArchiveCache for archive: " << pArchive->getName());
 
-bool createAbcArchiveCache( Abc::IArchive *pArchive, AbcArchiveCache* fullNameToObjectCache, CommonProgressBar *pBar ) {
-	ESS_PROFILE_SCOPE("createAbcArchiveCache");
-	EC_LOG_INFO( "Creating AbcArchiveCache for archive: " << pArchive->getName() );
+  runonce();
 
-	runonce();
-
-	Abc::IObject top = pArchive->getTop();
-	return addObjectToCache( fullNameToObjectCache, top, "", pBar ) != 0;
+  Abc::IObject top = pArchive->getTop();
+  return addObjectToCache(fullNameToObjectCache, top, "", pBar) != 0;
 }

Shared/CommonUtils/CommonImport.h
@@ -1,109 +1,119 @@
 #ifndef __COMMON_IMPORT_H
 #define __COMMON_IMPORT_H
 
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-#include "CommonSceneGraph.h"
-#include "CommonAlembic.h"
 #include "CommonAbcCache.h"
+#include "CommonAlembic.h"
+#include "CommonSceneGraph.h"
 
 #include "CommonPBar.h"
 #include "CommonRegex.h"
 
-namespace XSI_XformTypes{
-   enum xte{//Don't change the order! We write these out.
-      UNKNOWN,
-      XMODEL,
-      XNULL
-   };
+namespace XSI_XformTypes {
+enum xte {  // Don't change the order! We write these out.
+  UNKNOWN,
+  XMODEL,
+  XNULL
+};
 };
 
-namespace timeControlOptions{
-   enum tco{
-      NONE,
-      SCENE_ROOT,
-      ROOT_MODELS
-   };
+namespace timeControlOptions {
+enum tco { NONE, SCENE_ROOT, ROOT_MODELS };
 }
 
-class IJobStringParser
-{
-public:
+class IJobStringParser {
+ public:
+  bool importNormals;
+  bool importUVs;
+  bool importMaterialIds;
+  bool importFacesets;
+  bool importStandinProperties;
+  bool importBoundingBoxes;  // import boundng boxes instead of shape
+  bool attachToExisting;
+  bool failOnUnsupported;
+  bool importVisibilityControllers;
+  bool importTransformControllers;
+  bool selectShapes;
+  bool skipUnattachedNodes;
+  bool enableImportRootSelection;
+  bool stripMayaNamespaces;
+  bool importCurvesAsStrands;
+  bool useMultiFile;
+  bool enableSubD;
+  bool operatorCreationForExistingNodes;
 
-   bool importNormals;
-   bool importUVs;
-   bool importMaterialIds;
-   bool importFacesets;
-   bool importStandinProperties;
-   bool importBoundingBoxes;//import boundng boxes instead of shape
-   bool attachToExisting;
-   bool failOnUnsupported;
-   bool importVisibilityControllers;
-   bool importTransformControllers;
-   bool selectShapes;
-   bool skipUnattachedNodes;
-   bool enableImportRootSelection;
-   bool stripMayaNamespaces;
-   bool importCurvesAsStrands;
-   bool useMultiFile;
-   bool enableSubD;
-   bool operatorCreationForExistingNodes;
+  XSI_XformTypes::xte xformTypes;
+  timeControlOptions::tco timeControl;
 
-   XSI_XformTypes::xte xformTypes;
-   timeControlOptions::tco timeControl;
+  SearchReplace::ReplacePtr replacer;
 
-   SearchReplace::ReplacePtr replacer;
+  std::string filename;  // = EC_MCHAR_to_UTF8( strPath );
+  std::string prefix;
 
-	std::string filename;// = EC_MCHAR_to_UTF8( strPath );
-	std::string prefix;
+  std::vector<std::string> nodesToImport;
+  std::map<std::string, std::string> extraParameters;
 
-   std::vector<std::string> nodesToImport;
-   std::map<std::string, std::string> extraParameters;
+  bool includeChildren;
+  bool replaceColonsWithUnderscores;  // built-in option for XSI
 
-   bool includeChildren;
-   bool replaceColonsWithUnderscores; //built-in option for XSI
+  IJobStringParser()
+      : importNormals(false),
+        importUVs(true),
+        importMaterialIds(false),
+        importFacesets(true),
+        importStandinProperties(false),
+        importBoundingBoxes(false),
+        attachToExisting(false),
+        failOnUnsupported(false),
+        importVisibilityControllers(false),
+        importTransformControllers(false),
+        includeChildren(false),
+        selectShapes(true),
+        skipUnattachedNodes(false),
+        enableImportRootSelection(false),
+        stripMayaNamespaces(false),
+        importCurvesAsStrands(false),
+        replaceColonsWithUnderscores(false),
+        useMultiFile(false),
+        enableSubD(true),
+        operatorCreationForExistingNodes(true)
+  {
+  }
 
-   IJobStringParser():
-      importNormals(false),
-      importUVs(true),
-      importMaterialIds(false),
-      importFacesets(true),
-      importStandinProperties(false),
-      importBoundingBoxes(false),
-      attachToExisting(false),
-      failOnUnsupported(false),
-      importVisibilityControllers(false),
-	  importTransformControllers(false),
-      includeChildren(false),
-      selectShapes(true),
-      skipUnattachedNodes(false),
-      enableImportRootSelection(false),
-      stripMayaNamespaces(false),
-      importCurvesAsStrands(false),
-      replaceColonsWithUnderscores(false),
-	  useMultiFile(false),
-      enableSubD(true),
-      operatorCreationForExistingNodes(true)
-   {}
+  bool parse(const std::string& jobString);
 
-   bool parse(const std::string& jobString);
+  std::string buildJobString();
 
-   std::string buildJobString();
-
-   bool paramIsSet(const std::string& param);
-   void setParam(const std::string& param, bool bVal=true);
+  bool paramIsSet(const std::string& param);
+  void setParam(const std::string& param, bool bVal = true);
 };
 
-SceneNodeAlembicPtr buildAlembicSceneGraph(AbcArchiveCache *pArchiveCache, AbcObjectCache *pRootObjectCache, int& nNumNodes, const IJobStringParser& jobParams, bool countMergableChildren=true, CommonProgressBar *pBar = 0);
+SceneNodeAlembicPtr buildAlembicSceneGraph(AbcArchiveCache* pArchiveCache,
+                                           AbcObjectCache* pRootObjectCache,
+                                           int& nNumNodes,
+                                           const IJobStringParser& jobParams,
+                                           bool countMergableChildren = true,
+                                           CommonProgressBar* pBar = 0);
 
 // progress bar needs to be initialized before these functions are called!
-bool ImportSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot, const IJobStringParser& jobParams, CommonProgressBar *pBar = 0, std::list<SceneNodeAppPtr> *newNodes = 0);
-bool AttachSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot, const IJobStringParser& jobParams, CommonProgressBar *pBar = 0, std::list<SceneNodeAppPtr> *newNodes = 0);
-bool MergeSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot, const IJobStringParser& jobParams, CommonProgressBar *pBar = 0, std::list<SceneNodeAppPtr> *newNodes = 0);
+bool ImportSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot,
+                     const IJobStringParser& jobParams,
+                     CommonProgressBar* pBar = 0,
+                     std::list<SceneNodeAppPtr>* newNodes = 0);
+bool AttachSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot,
+                     const IJobStringParser& jobParams,
+                     CommonProgressBar* pBar = 0,
+                     std::list<SceneNodeAppPtr>* newNodes = 0);
+bool MergeSceneFile(SceneNodeAlembicPtr fileRoot, SceneNodeAppPtr appRoot,
+                    const IJobStringParser& jobParams,
+                    CommonProgressBar* pBar = 0,
+                    std::list<SceneNodeAppPtr>* newNodes = 0);
 
-
-void GetSampleRange(SceneNodeAlembicPtr fileRoot, std::size_t& oMinSample, std::size_t& oMaxSample, double& oMinTime, double& oMaxTime);
+void GetSampleRange(SceneNodeAlembicPtr fileRoot, std::size_t& oMinSample,
+                    std::size_t& oMaxSample, double& oMinTime,
+                    double& oMaxTime);
 
 #endif

Shared/CommonUtils/CommonAbcCache.h
@@ -1,7 +1,6 @@
 #ifndef __COMMON_ABC_CACHE_H__
 #define __COMMON_ABC_CACHE_H__
 
-
 #include <boost/smart_ptr.hpp>
 
 #include "CommonPBar.h"
@@ -9,33 +8,35 @@
 typedef boost::shared_ptr<AbcG::IXform> IXformPtr;
 
 class AbcObjectCache {
-protected:
+ protected:
+ public:
+  AbcObjectCache(Alembic::Abc::IObject &objToCache);
+  ~AbcObjectCache();
 
-public:
-	AbcObjectCache( Alembic::Abc::IObject & objToCache );
-    ~AbcObjectCache();
+  // AbcObjectCache(const AbcObjectCache& cache);
+  // AbcObjectCache& operator=(const AbcObjectCache& cache);
 
-    //AbcObjectCache(const AbcObjectCache& cache);
-    //AbcObjectCache& operator=(const AbcObjectCache& cache);
+  Abc::IObject obj;
+  int numSamples;
+  bool isConstant;
+  bool isMeshPointCache;
+  bool isMeshTopoDynamic;
+  std::vector<std::string> childIdentifiers;
+  std::string fullName;
+  std::string parentIdentifier;
 
-	Abc::IObject obj;
-	int numSamples;
-	bool isConstant;
-	bool isMeshPointCache;
-	bool isMeshTopoDynamic;
-	std::vector<std::string> childIdentifiers;
-	std::string fullName;
-	std::string parentIdentifier;
+  IXformPtr getXform();
+  Abc::M44d getXformMatrix(int index);
 
-    IXformPtr getXform();
-    Abc::M44d getXformMatrix(int index);
-private:
-    IXformPtr pObjXform;
-    std::map<int, Abc::M44d> iXformMap;
+ private:
+  IXformPtr pObjXform;
+  std::map<int, Abc::M44d> iXformMap;
 };
 
-typedef std::map<std::string,AbcObjectCache> AbcArchiveCache;
+typedef std::map<std::string, AbcObjectCache> AbcArchiveCache;
 
-bool createAbcArchiveCache( Abc::IArchive *pArchive, AbcArchiveCache* fullNameToObjectCache, CommonProgressBar *pBar = 0 );
+bool createAbcArchiveCache(Abc::IArchive *pArchive,
+                           AbcArchiveCache *fullNameToObjectCache,
+                           CommonProgressBar *pBar = 0);
 
-#endif // __COMMON_ABC_CACHE_H__
+#endif  // __COMMON_ABC_CACHE_H__

Shared/CommonUtils/CommonAlembic.h
@@ -1,69 +1,68 @@
 #ifndef __COMMON_ALEMBIC_H
 #define __COMMON_ALEMBIC_H
 
-
-#include <utility>
 #include <limits>
+#include <utility>
 
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <list>
+#include <map>
 #include <set>
 #include <vector>
-#include <map>
-#include <list>
-#include <deque>
-#include <cmath>
-#include <algorithm>
 
-#include <stdexcept>
 #include <exception>
+#include <stdexcept>
 
-#include <string>
-#include <sstream>
-#include <fstream>
 #include <cstring>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-#include <time.h>
-#include <math.h>
-#include <float.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/cstdint.hpp>
-#include <boost/smart_ptr.hpp>
-#include <boost/format.hpp>
-#include <boost/variant.hpp>
 #include <boost/exception/all.hpp>
-#include <boost/lexical_cast.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/smart_ptr.hpp>
 #include <boost/thread/mutex.hpp>
+#include <boost/variant.hpp>
 
 namespace fs = boost::filesystem;
 
 #include <ImathMatrixAlgo.h>
 
 #include <Alembic/Abc/All.h>
-#include <Alembic/AbcGeom/All.h>
+#include <Alembic/AbcCoreAbstract/TimeSampling.h>
+#include <Alembic/AbcCoreFactory/All.h>
 #include <Alembic/AbcCoreHDF5/All.h>
 #include <Alembic/AbcCoreOgawa/All.h>
-#include <Alembic/AbcCoreFactory/All.h>
-#include <Alembic/AbcCoreAbstract/TimeSampling.h>
-#include <Alembic/Util/Murmur3.h>
+#include <Alembic/AbcGeom/All.h>
 #include <Alembic/AbcMaterial/All.h>
+#include <Alembic/Util/Murmur3.h>
 
 namespace Alembic {
-	namespace Abc {
-		namespace ALEMBIC_VERSION_NS {
-			using Imath::V4s;
-			using Imath::V4i;
-			using Imath::V4f;
-			using Imath::V4d;
-		}
-	}
+namespace Abc {
+namespace ALEMBIC_VERSION_NS {
+using Imath::V4s;
+using Imath::V4i;
+using Imath::V4f;
+using Imath::V4d;
+}
+}
 }
 
 namespace AbcA = ::Alembic::AbcCoreAbstract::ALEMBIC_VERSION_NS;
@@ -73,8 +72,8 @@ namespace AbcG = ::Alembic::AbcGeom::ALEMBIC_VERSION_NS;
 namespace AbcU = ::Alembic::Util::ALEMBIC_VERSION_NS;
 namespace AbcM = ::Alembic::AbcMaterial::ALEMBIC_VERSION_NS;
 
-#include "CommonOS.h"
 #include "CommonLog.h"
+#include "CommonOS.h"
 #include "CommonProfiler.h"
 
-#endif // __COMMON_ALEMBIC_H
+#endif  // __COMMON_ALEMBIC_H

commit c37da6585e7af07e568b9b841c18610732c97ea6

fixes build break for 'CommonUtils' introduced in 966e604 : 'CommonAlembic.h' must always be included before 'CommonAbcCache.h'

Shared/CommonUtils/CommonAbcCache.cpp
@@ -1,5 +1,5 @@
-#include "CommonAbcCache.h"
 #include "CommonAlembic.h"
+#include "CommonAbcCache.h"
 #include "CommonMeshUtilities.h"
 #include "CommonUtilities.h"
 

Shared/CommonUtils/CommonImport.h
@@ -5,8 +5,8 @@
 #include <string>
 #include <vector>
 
-#include "CommonAbcCache.h"
 #include "CommonAlembic.h"
+#include "CommonAbcCache.h"
 #include "CommonSceneGraph.h"
 
 #include "CommonPBar.h"

Shared/CommonUtils/CommonUtilities.h
@@ -1,8 +1,8 @@
 #ifndef __COMMON_UTILITIES_H
 #define __COMMON_UTILITIES_H
 
-#include "CommonAbcCache.h"
 #include "CommonAlembic.h"
+#include "CommonAbcCache.h"
 
 #include "CommonPBar.h"
 